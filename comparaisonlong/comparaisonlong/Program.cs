010a49f632d31b59766187bc486d68c619d66431
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:56:33 -0400

    Add files via upload

---
+++ comparaisonlong/comparaisonlong/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comparaisonlongueur
+{
+    class Program
+    {
+
+        public static bool comparerchaine(string p_chaine1, string p_chaine2)
+        {
+            if (p_chaine1.Length == p_chaine2.Length)
+                return true;
+            else return false;
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(comparerchaine("hasnaa", "Didier"));
+            Console.WriteLine(comparerchaine("hasnaa", "Didiere"));
+            Console.ReadLine();
+        }
+    }
+}
